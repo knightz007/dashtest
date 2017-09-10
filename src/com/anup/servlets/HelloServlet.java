a0ebfffe521e9f84f9c4922a7084e059580f5051
knightz007 <anupkumar.menon@yahoo.com>
2017-09-10 16:56:44 -0700

    Update hello servlet for splitting string

--- src/com/anup/servlets/HelloServlet.java
+++ src/com/anup/servlets/HelloServlet.java
@@ -3,8 +3,14 @@ package com.anup.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.RequestDispatcher;
@@ -35,44 +41,103 @@ public class HelloServlet extends GenericServlet {
 			Connection conn = DbConnector.getConnection();
 			out.println("Connection Successful");
 			
-			String env = queryDB.findServer(conn, "myserver1");
-			out.println(env);
+//			String env = queryDB.findServer(conn, "myserver1");
+//			out.println(env);
+//			
+//			List<ServerInfo> serverList = queryDB.getServerList();
+//			
+//			for (ServerInfo server : serverList) {
+//				out.println(server.getHostname() + "::" + server.getEnvironment());
+//			}
+//			
+//			out.println("hello");
 			
-			List<ServerInfo> serverList = queryDB.getServerList();
-			
-			for (ServerInfo server : serverList) {
-				out.println(server.getHostname() + "::" + server.getEnvironment());
-			}
-			
-			request.setAttribute("serverList", serverList);			
+			/*request.setAttribute("serverList", serverList);			
 			request.setAttribute("htmlTagData", "<br/> creates a new line.");
 			request.setAttribute("url", "http://www.testjstl.com");		
+			*/
 			
+			String sql = "SELECT hostname, components_version FROM testdb.serverlist where environment='stage' and components_version like '%astra%'";
 			
-			String serverJson = getJson.getServerListJSON();
-			response.getWriter().write(serverJson);
-			//System.out.println(serverJson);
-			 // convert your list to json
-			 //String jsonServerList = gson.toJson(serverList);
-			 // print your generated json
-			 //System.out.println("jsonCartList: " + jsonServerList);
-			
-			//Gson gson = new GsonBuilder().create();
-	        //gson.toJson("Hello", System.out);
-	        //gson.toJson(123, System.out);
-			
-			//String json = new Gson().toJson(serverList);
-			//out.println(json);
-			//RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/ServerListView.jsp");
-		   // dispatcher.forward(request, response);	    
+			   PreparedStatement pstm = conn.prepareStatement(sql);
+			      ResultSet rs = pstm.executeQuery();
+			      
+			      StringBuilder componentsVersionStr = new StringBuilder();
+			      
+			      while(rs.next()) {
+			    	  componentsVersionStr.append(rs.getString("components_version"));
+			    	  componentsVersionStr.append(" ");
+			      }
+			      
+			      //out.print("String is:" + componentsVersionStr.toString());			      
+			      
+			      
+			      String[] users = componentsVersionStr.toString().split(" ");
+			      List<String> componentVersionList = new LinkedList<String>();
+			      List<String> components = new LinkedList<String>();
+			      //StringBuilder components = new StringBuilder();
+			      String[] comp;
+			      for (int i = 0; i < users.length; i++)
+			      {
+			    	 componentVersionList.add(users[i].toString());
+			    	 comp = users[i].toString().split(":");
+			    	 components.add(comp[0].toString());		    	 
+			      }
+			      
+			      out.println("Number of non unique components:" + components.size());
+			      
+			      Set<String> uniqueComponents = new HashSet<String>(components);
+			      out.println("Number of unique components:" + uniqueComponents.size());
+			      
+			      Set<String> uniqueComponentVersion = new HashSet<String>(componentVersionList);
+			      out.println("Number of unique component versions:" + uniqueComponentVersion.size());
+			      
+			      
+//			      Set<String> uniqueComponents = new HashSet<String>();
+//
+//			        for (int i = 0; i < users.length; i++) {
+//			            if (!uniqueComponents.add(users[i]))
+//			            	
+//			        }
+			      
+			      
+			      //String[] users = "User1,User2,User1,User,User".split(",");
+
+			        Set<String> uniquUsers = new HashSet<String>();
+
+			        for (int i = 0; i < users.length; i++) {
+			            if (!uniquUsers.add(users[i]))
+			                users[i] = "Duplicate"; // here I am assigning Duplicate instead if find duplicate
+			                                        // you can assign as null or whatever you want to do with duplicates.
+			        }
+			        out.println(Arrays.toString(users));
+			      
+			      			      
+			      /*for(String s: componentVersions)
+			      {
+			    	 
+			      }
+			      
+			      String componentVersion = componentVersions[0]; */
+			      //out.println(componentVersion);
+			      
+			      
+			      
+			     //Check Component for version sync
+/*			      List<String> comp_version_list =   new LinkedList<String>(); ;
+			      while(rs.next()) {
+			    	 comp_version_list.add(rs.getString("components_version"));
+			      }	*/		      
+			      
+			      //out.println("Size is:" + comp_version_list.size());
 			
 			
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			out.println(e.getMessage());
+			//e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			out.println(e.getMessage());			
+			//e.printStackTrace();
 		}
 
 	}
